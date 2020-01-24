2f04ed95b817aadc998fce0f776db731da957480
ellfis <ellfis@hotmail.com>
2020-01-23 19:24:32 -0500

    Update psychopathy.sh

--- psychopathy.sh
+++ psychopathy.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 function stop {
-	bash ./psychopathy
+	bash ./psychopathy.sh
 }
 
 trap stop INT TSTP QUIT TERM STOP
 
 emacs -Q -f doctor
-bash ./psychopathy
+bash ./psychopathy.sh
